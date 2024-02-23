40ee6f63639116416adb604002af559da952ae2d
Alchemmist <anton.ingrish@gmail.com>
2024-02-23 16:09:02 +0300

    add error handling

--- api.py
+++ api.py
@@ -1,16 +1,40 @@
 import config
-from requests import get
+from typing import Callable
+from rich.prompt import Prompt
+from rich import print
+from requests import get, exceptions
 from view import console
 
 
+def error_loginig(func: Callable) -> Callable:
+    def wrapper():
+        try:
+            return func()
+        except exceptions.ConnectionError:
+            print("[b red]Упс! Проверьте ваше интернет соединение[/]")
+            answer = Prompt.ask("Хотите посмотреть всю ошибку?", choices=["y", "n"], default="n")
+            if answer == "y":
+                console.print_exception(max_frames=1)
+            exit()
+        except Exception:
+            print("[b red]Упс! Что-то сломалось, не смог получить координаты[/]")
+            answer = Prompt.ask("Хотите посмотреть всю ошибку?", choices=["y", "n"], default="n")
+            if answer == "y":
+                console.print_exception(max_frames=1)
+            exit()
+    return wrapper
+
+
+@error_loginig
 def get_coordinates() -> tuple:
     with console.status("Получаем координаты...", spinner="aesthetic"):
         response = get("http://ipinfo.io/json").json()
     lat = response["loc"].split(",")[0]
-    lon = response["loc"].split(",")[1]
+    lon = response["loc"].split(",")[1]    
     return lat, lon
 
 
+@error_loginig
 def get_weather_now() -> dict:
     params = config.BASIC_REQUEST_PARAMS.copy()
     params["lat"], params["lon"] = get_coordinates()
@@ -22,6 +46,7 @@ def get_weather_now() -> dict:
     return response
 
 
+@error_loginig
 def get_weather_forecast() -> dict:
     params = config.BASIC_REQUEST_PARAMS.copy()
     params["lat"], params["lon"] = get_coordinates()
